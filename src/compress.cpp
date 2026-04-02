e5c6af98d8d6bd2a15e41b208f213678c5251e4a
Simon Ambrozak <sambroza@nexuscbcb00.umiacs.umd.edu>
2026-04-02 12:44:44 -0400

    added starting code

---
+++ src/compress.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <cstring>
+
+using namespace std;
+
+
+int main(int argc, char* argv[]) {
+
+    if (argc != 3) {
+        printf("Use: compress in_ref.fa out_compressed.fa\n");
+        return 1;
+    }
+
+    // Load
+
+    // Compress
+
+    // Write out
+    
+    return 0;
+}
---
+++ src/query.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <cstring>
+
+using namespace std;
+
+
+int main(int argc, char* argv[]) {
+
+    if (argc != 3) {
+        printf("Use: compress reference_compressed.fa queries.fa\n");
+        return 1;
+    }
+
+    // Load ref
+
+    // Load and search for queries
+
+    // Write querying results
+    
+    return 0;
+}
